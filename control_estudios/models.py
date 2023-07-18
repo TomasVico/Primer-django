1326e0c92013ce5ff9e7cd09aee33ca99d07ca61
Tomas Vico <tvico97@hotmail.com>
2023-07-18 17:08:24 -0300

    repositorio clonado

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    nombre= models.CharField(max_lenght=64)
+    comision= models.IntegerField()
+
+
+class Estudiante(models.Model):
+    nombre= models.CharField(max_length=256)
+    apellido= models.CharField(max_length=256)
+    email=models.EmailField(blank=True)
+    telefono=models.CharField(max_length=50,blank=True)
+    dni=models.CharField(max_length=32)
+    fecha_nacimiento=models.DateField(null=True)
+
+class Profesor(models.Model):
+    nombre= models.CharField(max_length=256)
+    apellido= models.CharField(max_length=256)
+    email=models.EmailField(blank=True)
+    telefono=models.CharField(max_length=50,blank=True)
+    dni=models.CharField(max_length=32)
+    fecha_nacimiento=models.DateField(null=True)
+    profesion=models.CharField(max_length=256)
+    bio=models.TextField(blank=True)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+    esta_aprobado = models.BooleanField(default=False) 
+
